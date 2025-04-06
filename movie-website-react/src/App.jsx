889363aa5b403216073cdb441ea6d8533d151092
Umee7 <umeshmessi23@gmail.com>
2025-04-06 23:56:01 +0530

    added Header Section with Search Bar

--- movie-website-react/src/App.jsx
+++ movie-website-react/src/App.jsx
@@ -1,10 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Search from './components/Search'
 
+const API_BASE_URL = 'https://api.themoviedb.org/3/';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
+  
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const fetchMovies = async () => {
+    try {
+
+    } catch (error) {
+      console.log(`Error fetching movies: ${error}`);
+      setErrorMessage('Error fetching movies. please try again later.')
+    }
+  }
 
   return ( 
     <main>
@@ -12,13 +34,16 @@ function App() {
 
         <div className="wrapper">
           <header>
-          <img src="./hero.png " alt="" />
+          <img src="./hero.png " alt="image" draggable="false" />
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
-          </header>
-
+          
           <Search searchTerm={searchTerm}  setSearchTerm={setSearchTerm} />
-
-          <h1>{searchTerm}</h1>
+          </header>
+          
+          <section className="all-movies">
+            <h2>All Movies</h2>
+  
+          </section>
         </div>
     </main>
   )
